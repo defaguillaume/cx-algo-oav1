1d42ddbf712e0b334c3db0391f080b60fc948775
defaguillaume <guillaumedefaye9@gmail.com>
2020-10-14 14:52:16 +0200

    exo3

---
+++ e03.js
@@ -0,0 +1,30 @@
+/*
+03 Marsos
+Create a function called marsos that will take in one string str as input.
+A spaceship is sent to Mars but crashes.
+It sends a series of “SOS” messages to Earth for help.
+Due to cosmic radiation, some of the “SOS” messages were altered during transmission.
+
+The goal of the function is to determine how many letters were altered during transmission.
+
+Exemple: for the input SOFS OSS ISSOW the function will return 3
+*/
+
+
+function Marsos (str) {
+
+    
+    const alphabet = ["SOS", "SOS", "SOS"]
+    let count = 0
+    for (var i = 0; i < alphabet.length; i++) {
+        const loweredSentence = str.toLowerCase(i)
+    
+        if (!loweredSentence.includes(alphabet)) {
+         count ++
+        }
+      }
+  
+    return count
+}
+const count= Marsos("SOFSOSSISSOW")
+console.log(count)
